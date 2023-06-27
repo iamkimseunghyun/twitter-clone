8a0c004b2792a5d7569142a9f663399386e84774
iamkimseunghyun <iamkimseunghyun@gmail.com>
2023-06-27 20:26:21 +0900

    Load and create posts done by 3:23:20

---
+++ components/posts/PostFeed.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import usePosts from '@/hooks/usePosts';
+import PostItem from '@/components/posts/PostItem';
+
+interface PostFeedProps {
+  userId?: string;
+}
+const PostFeed: React.FC<PostFeedProps> = ({ userId }) => {
+  const { data: posts = [] } = usePosts(userId);
+  return (
+    <div>
+      {posts.map((post: Record<string, any>) => (
+        <PostItem userId={userId} key={post.id} data={post} />
+      ))}
+    </div>
+  );
+};
+
+export default PostFeed;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,19 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import Header from "@/components/Header";
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import styles from '@/styles/Home.module.css';
+import Header from '@/components/Header';
+import Form from '@/components/Form';
+import PostFeed from '@/components/posts/PostFeed';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <>
-     <Header label="Home"></Header>
+      <Header label="Home"></Header>
+      <Form placeholder="What's happening?" />
+      <PostFeed />
     </>
-  )
+  );
 }
